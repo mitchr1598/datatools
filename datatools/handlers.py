f9f89662f8c8149d4cc6250243453a00a7e09df7
mitchr1598 <a1688803@student.adelaide.edu.au>
2023-02-16 15:48:12 +1030

    added an upload_data in the the datahandler

--- datatools/handlers.py
+++ datatools/handlers.py
@@ -33,12 +33,18 @@ class DataHandler:
             self.transformer = transformer
         self.transformed_data = self.transformer.transform(self.imported_data)
 
-    def export_data(self, uploader: uploading.uploaders.Uploader = None, *args, **kwargs) -> pd.DataFrame:
-        if self.transformed_data.empty:
-            self.transform_data()
+    def upload_data(self, uploader: uploading.Uploader = None, *args, **kwargs) -> None:
         if uploader is not None:
             self.uploader = uploader
         self.uploader.upload_data(self.transformed_data)
+
+    def export_data(self, uploader: uploading.uploaders.Uploader = None, *args, **kwargs) -> pd.DataFrame:
+        """
+        Get's the success uploads. Will run both the transform and upload methods if they haven't been run yet.
+        """
+        if self.transformed_data.empty:
+            self.transform_data()
+        self.upload_data(uploader)
         self.exported_data, self.failed_exports = self.uploader.get_successful_uploads(), self.uploader.get_failed_uploads()
         return self.exported_data
 
